e140ee1589b5b643ca64d55e6bc0d8a0e0045cf8
devozs <oz.shemesh@datomize.com>
2023-11-04 18:02:23 +0200

    update content

--- lang/en-US.ts
+++ lang/en-US.ts
@@ -4,8 +4,8 @@ export default {
         sub_title: "Remember the falling of October 07",
         latest: "Latest Memorials",
         thier_stories: "The Green Heroes",
-        thier_stories_1: "TBD",
-        thier_stories_2: "TBD",
+        thier_stories_1: "Our heart is broken. We part from brothers and sisters to the stand, you will always be a part of us, always in our heart.",
+        thier_stories_2: "We will not forget you.",
         thier_stories_3: "TBD",
         footer: "🇮🇱 In memory of the victims of October 7, 2023 🇮🇱"
     },
